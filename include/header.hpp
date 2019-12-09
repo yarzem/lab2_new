b5c57c103f35590e312c4fb48bb77b3d96c92415
Sergei2000 <dsdem@mail.ru>
2019-12-10 00:16:46 +0300

    try3

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,52 @@
-// Copyright 2018 Your Name <your_email>
-
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+
+#include <iostream>
+#include <ctime>
+#include <map>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+#include <string>
+
+#define l1 393216
+#define  l3 9437184
+#define iterations 1000
+#define translate 1024*1024
+typedef std::chrono::high_resolution_clock Clock;
+
+
+class Cache {
+public:
+    Cache();
+
+    std::map<int, int> number_of_experiments();
+
+    void straight_pass();
+
+    void back_pass();
+
+    void random_pass();
+
+    void resultWindow(std::string const &, size_t const &, int const &,
+                      double const &);
+
+    ~Cache() {}
+
+private:
+    void set_array(int);
+
+    void set_straight(int);
+
+    void set_back(int);
+
+    void set_random(int, std::vector <size_t> &);
+
+
+public:
+    int *_array;
+    std::map<int, int> _experiments;
+};
 
 #endif // INCLUDE_HEADER_HPP_
